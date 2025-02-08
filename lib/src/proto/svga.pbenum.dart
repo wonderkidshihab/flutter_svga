10eb3324e6f97bae52abb5ee00b2a23996fd0bf3
pen10 <5alafawyyy.work@gmail.com>
2025-02-08 22:59:04 +0200

    Refactor enum names in svga.pbenum.dart to use uppercase constants for consistency

--- lib/src/proto/svga.pbenum.dart
+++ lib/src/proto/svga.pbenum.dart
@@ -6,26 +6,25 @@
 
 // ignore_for_file: UNDEFINED_SHOWN_NAME
 import 'dart:core' as $core;
-
 import 'package:protobuf/protobuf.dart' as $pb;
 
 class ShapeEntity_ShapeType extends $pb.ProtobufEnum {
-  static const ShapeEntity_ShapeType shape = ShapeEntity_ShapeType._(
+  static const ShapeEntity_ShapeType SHAPE = ShapeEntity_ShapeType._(
       0,
       const $core.bool.fromEnvironment('protobuf.omit_enum_names')
           ? ''
           : 'SHAPE');
-  static const ShapeEntity_ShapeType rect = ShapeEntity_ShapeType._(
+  static const ShapeEntity_ShapeType RECT = ShapeEntity_ShapeType._(
       1,
       const $core.bool.fromEnvironment('protobuf.omit_enum_names')
           ? ''
           : 'RECT');
-  static const ShapeEntity_ShapeType ellipse = ShapeEntity_ShapeType._(
+  static const ShapeEntity_ShapeType ELLIPSE = ShapeEntity_ShapeType._(
       2,
       const $core.bool.fromEnvironment('protobuf.omit_enum_names')
           ? ''
           : 'ELLIPSE');
-  static const ShapeEntity_ShapeType keep = ShapeEntity_ShapeType._(
+  static const ShapeEntity_ShapeType KEEP = ShapeEntity_ShapeType._(
       3,
       const $core.bool.fromEnvironment('protobuf.omit_enum_names')
           ? ''
@@ -33,33 +32,33 @@ class ShapeEntity_ShapeType extends $pb.ProtobufEnum {
 
   static const $core.List<ShapeEntity_ShapeType> values =
       <ShapeEntity_ShapeType>[
-    shape,
-    rect,
-    ellipse,
-    keep,
+    SHAPE,
+    RECT,
+    ELLIPSE,
+    KEEP,
   ];
 
   static final $core.Map<$core.int, ShapeEntity_ShapeType> _byValue =
       $pb.ProtobufEnum.initByValue(values);
   static ShapeEntity_ShapeType? valueOf($core.int value) => _byValue[value];
 
-  const ShapeEntity_ShapeType._(super.v, super.n);
+  const ShapeEntity_ShapeType._($core.int v, $core.String n) : super(v, n);
 }
 
 class ShapeEntity_ShapeStyle_LineCap extends $pb.ProtobufEnum {
-  static const ShapeEntity_ShapeStyle_LineCap lineCapButt =
+  static const ShapeEntity_ShapeStyle_LineCap LineCap_BUTT =
       ShapeEntity_ShapeStyle_LineCap._(
           0,
           const $core.bool.fromEnvironment('protobuf.omit_enum_names')
               ? ''
               : 'LineCap_BUTT');
-  static const ShapeEntity_ShapeStyle_LineCap lineCapROUND =
+  static const ShapeEntity_ShapeStyle_LineCap LineCap_ROUND =
       ShapeEntity_ShapeStyle_LineCap._(
           1,
           const $core.bool.fromEnvironment('protobuf.omit_enum_names')
               ? ''
               : 'LineCap_ROUND');
-  static const ShapeEntity_ShapeStyle_LineCap lineCapSQUARE =
+  static const ShapeEntity_ShapeStyle_LineCap LineCap_SQUARE =
       ShapeEntity_ShapeStyle_LineCap._(
           2,
           const $core.bool.fromEnvironment('protobuf.omit_enum_names')
@@ -68,9 +67,9 @@ class ShapeEntity_ShapeStyle_LineCap extends $pb.ProtobufEnum {
 
   static const $core.List<ShapeEntity_ShapeStyle_LineCap> values =
       <ShapeEntity_ShapeStyle_LineCap>[
-    lineCapButt,
-    lineCapROUND,
-    lineCapSQUARE,
+    LineCap_BUTT,
+    LineCap_ROUND,
+    LineCap_SQUARE,
   ];
 
   static final $core.Map<$core.int, ShapeEntity_ShapeStyle_LineCap> _byValue =
@@ -78,23 +77,24 @@ class ShapeEntity_ShapeStyle_LineCap extends $pb.ProtobufEnum {
   static ShapeEntity_ShapeStyle_LineCap? valueOf($core.int value) =>
       _byValue[value];
 
-  const ShapeEntity_ShapeStyle_LineCap._(super.v, super.n);
+  const ShapeEntity_ShapeStyle_LineCap._($core.int v, $core.String n)
+      : super(v, n);
 }
 
 class ShapeEntity_ShapeStyle_LineJoin extends $pb.ProtobufEnum {
-  static const ShapeEntity_ShapeStyle_LineJoin lineJoinMITER =
+  static const ShapeEntity_ShapeStyle_LineJoin LineJoin_MITER =
       ShapeEntity_ShapeStyle_LineJoin._(
           0,
           const $core.bool.fromEnvironment('protobuf.omit_enum_names')
               ? ''
               : 'LineJoin_MITER');
-  static const ShapeEntity_ShapeStyle_LineJoin lineJoinROUND =
+  static const ShapeEntity_ShapeStyle_LineJoin LineJoin_ROUND =
       ShapeEntity_ShapeStyle_LineJoin._(
           1,
           const $core.bool.fromEnvironment('protobuf.omit_enum_names')
               ? ''
               : 'LineJoin_ROUND');
-  static const ShapeEntity_ShapeStyle_LineJoin lineJoinBEVEL =
+  static const ShapeEntity_ShapeStyle_LineJoin LineJoin_BEVEL =
       ShapeEntity_ShapeStyle_LineJoin._(
           2,
           const $core.bool.fromEnvironment('protobuf.omit_enum_names')
@@ -103,9 +103,9 @@ class ShapeEntity_ShapeStyle_LineJoin extends $pb.ProtobufEnum {
 
   static const $core.List<ShapeEntity_ShapeStyle_LineJoin> values =
       <ShapeEntity_ShapeStyle_LineJoin>[
-    lineJoinMITER,
-    lineJoinROUND,
-    lineJoinBEVEL,
+    LineJoin_MITER,
+    LineJoin_ROUND,
+    LineJoin_BEVEL,
   ];
 
   static final $core.Map<$core.int, ShapeEntity_ShapeStyle_LineJoin> _byValue =
@@ -113,5 +113,6 @@ class ShapeEntity_ShapeStyle_LineJoin extends $pb.ProtobufEnum {
   static ShapeEntity_ShapeStyle_LineJoin? valueOf($core.int value) =>
       _byValue[value];
 
-  const ShapeEntity_ShapeStyle_LineJoin._(super.v, super.n);
+  const ShapeEntity_ShapeStyle_LineJoin._($core.int v, $core.String n)
+      : super(v, n);
 }
